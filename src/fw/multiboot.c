6e87c8c27014f2e79db9b1ba932541c5980bbd79
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-25 17:06:22 -0400

    multiboot: import GRUB framebuffer for VGA BIOS

--- src/fw/multiboot.c
+++ src/fw/multiboot.c
@@ -167,6 +167,136 @@ mbfs_copyfile(struct romfile_s *file, void *dst, u32 maxlen)
     return size;
 }
 
+// SeaVGABIOS's coreboot framebuffer backend is already able to provide INT10
+// and VBE services on top of a linear framebuffer.  The VGA option ROM is a
+// separate binary, though, so it cannot read entry_elf_ebx directly.  Publish
+// GRUB's Multiboot framebuffer as the small coreboot table that backend already
+// understands.  0x500 is outside the IVT/BDA and is only populated after all
+// Multiboot modules have been copied away from the bootloader handoff data.
+#define MULTIBOOT_CB_TABLE_ADDR 0x500
+#define CB_SIGNATURE 0x4f49424c // "LBIO"
+#define CB_TAG_FRAMEBUFFER 0x0012
+
+struct multiboot_cb_header {
+    u32 signature;
+    u32 header_bytes;
+    u32 header_checksum;
+    u32 table_bytes;
+    u32 table_checksum;
+    u32 table_entries;
+};
+
+struct multiboot_cb_framebuffer {
+    u32 tag;
+    u32 size;
+    u64 physical_address;
+    u32 x_resolution;
+    u32 y_resolution;
+    u32 bytes_per_line;
+    u8 bits_per_pixel;
+    u8 red_mask_pos;
+    u8 red_mask_size;
+    u8 green_mask_pos;
+    u8 green_mask_size;
+    u8 blue_mask_pos;
+    u8 blue_mask_size;
+    u8 reserved_mask_pos;
+    u8 reserved_mask_size;
+};
+
+struct multiboot_cb_table {
+    struct multiboot_cb_header header;
+    struct multiboot_cb_framebuffer framebuffer;
+};
+
+static u16
+multiboot_ipchksum(void *buf, int count)
+{
+    u16 *p = buf;
+    u32 sum = 0;
+    while (count > 1) {
+        sum += *p++;
+        count -= 2;
+    }
+    if (count)
+        sum += *(u8 *)p;
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += sum >> 16;
+    return ~sum;
+}
+
+static void
+multiboot_prepare_vga(struct multiboot_info *mbi)
+{
+    if (!(mbi->flags & MULTIBOOT_INFO_FRAMEBUFFER_INFO)
+        || mbi->framebuffer_type != MULTIBOOT_FRAMEBUFFER_TYPE_RGB
+        || !mbi->framebuffer_addr || mbi->framebuffer_addr > 0xffffffff
+        || !mbi->framebuffer_pitch || !mbi->framebuffer_width
+        || !mbi->framebuffer_height)
+        return;
+
+    u8 bpp = mbi->framebuffer_bpp;
+    if (bpp != 15 && bpp != 16 && bpp != 24 && bpp != 32)
+        return;
+
+    u8 rpos = mbi->framebuffer_red_field_position;
+    u8 rsize = mbi->framebuffer_red_mask_size;
+    u8 gpos = mbi->framebuffer_green_field_position;
+    u8 gsize = mbi->framebuffer_green_mask_size;
+    u8 bpos = mbi->framebuffer_blue_field_position;
+    u8 bsize = mbi->framebuffer_blue_mask_size;
+    if (!rsize || !gsize || !bsize
+        || rpos + rsize > bpp || gpos + gsize > bpp || bpos + bsize > bpp)
+        return;
+
+    u32 bypp = DIV_ROUND_UP(bpp, 8);
+    u64 minimum_pitch = (u64)mbi->framebuffer_width * bypp;
+    u64 framebuffer_size = (u64)mbi->framebuffer_pitch
+                           * mbi->framebuffer_height;
+    if (mbi->framebuffer_pitch < minimum_pitch
+        || mbi->framebuffer_addr + framebuffer_size > 0x100000000ULL)
+        return;
+
+    u8 used_end = rpos + rsize;
+    if (used_end < gpos + gsize)
+        used_end = gpos + gsize;
+    if (used_end < bpos + bsize)
+        used_end = bpos + bsize;
+    u8 reserved_size = used_end < bpp ? bpp - used_end : 0;
+
+    struct multiboot_cb_table *table = (void *)MULTIBOOT_CB_TABLE_ADDR;
+    memset(table, 0, sizeof(*table));
+    table->header.signature = CB_SIGNATURE;
+    table->header.header_bytes = sizeof(table->header);
+    table->header.table_bytes = sizeof(table->framebuffer);
+    table->header.table_entries = 1;
+
+    table->framebuffer.tag = CB_TAG_FRAMEBUFFER;
+    table->framebuffer.size = sizeof(table->framebuffer);
+    table->framebuffer.physical_address = mbi->framebuffer_addr;
+    table->framebuffer.x_resolution = mbi->framebuffer_width;
+    table->framebuffer.y_resolution = mbi->framebuffer_height;
+    table->framebuffer.bytes_per_line = mbi->framebuffer_pitch;
+    table->framebuffer.bits_per_pixel = bpp;
+    table->framebuffer.red_mask_pos = rpos;
+    table->framebuffer.red_mask_size = rsize;
+    table->framebuffer.green_mask_pos = gpos;
+    table->framebuffer.green_mask_size = gsize;
+    table->framebuffer.blue_mask_pos = bpos;
+    table->framebuffer.blue_mask_size = bsize;
+    table->framebuffer.reserved_mask_pos = used_end;
+    table->framebuffer.reserved_mask_size = reserved_size;
+
+    table->header.table_checksum = multiboot_ipchksum(
+        &table->framebuffer, sizeof(table->framebuffer));
+    table->header.header_checksum = multiboot_ipchksum(
+        &table->header, sizeof(table->header));
+
+    dprintf(1, "Using Multiboot framebuffer @ %llx %dx%d with %d bpp (%d stride).\n",
+            mbi->framebuffer_addr, mbi->framebuffer_width,
+            mbi->framebuffer_height, bpp, mbi->framebuffer_pitch);
+}
+
 void
 multiboot_init(void)
 {
@@ -177,40 +307,42 @@ multiboot_init(void)
     dprintf(1, "mbptr=%p\n", mbi);
     dprintf(1, "flags=0x%x, mods=0x%x, mods_c=%d\n", mbi->flags, mbi->mods_addr,
             mbi->mods_count);
-    if (!(mbi->flags & MULTIBOOT_INFO_MODS))
-        return;
-    int i;
-    struct multiboot_mod_list *mod = (void *)mbi->mods_addr;
-    for (i = 0; i < mbi->mods_count; i++) {
-        struct mbfs_romfile_s *cfile;
-        u8 *copy;
-        u32 len;
-        if (!mod[i].cmdline)
-            continue;
-        len = mod[i].mod_end - mod[i].mod_start;
-        cfile = malloc_tmp(sizeof(*cfile));
-        if (!cfile) {
-            warn_noalloc();
-            return;
+    if (mbi->flags & MULTIBOOT_INFO_MODS) {
+        int i;
+        struct multiboot_mod_list *mod = (void *)mbi->mods_addr;
+        for (i = 0; i < mbi->mods_count; i++) {
+            struct mbfs_romfile_s *cfile;
+            u8 *copy;
+            u32 len;
+            if (!mod[i].cmdline)
+                continue;
+            len = mod[i].mod_end - mod[i].mod_start;
+            cfile = malloc_tmp(sizeof(*cfile));
+            if (!cfile) {
+                warn_noalloc();
+                return;
+            }
+            memset(cfile, 0, sizeof(*cfile));
+            dprintf(1, "module %s, size 0x%x\n", (char *)mod[i].cmdline, len);
+            if (!extract_filename(cfile->file.name, (char *)mod[i].cmdline,
+                                  sizeof(cfile->file.name))) {
+                free(cfile);
+                continue;
+            }
+            dprintf(1, "assigned file name <%s>\n", cfile->file.name);
+            cfile->file.size = len;
+            copy = malloc_tmp(len);
+            if (!copy) {
+                warn_noalloc();
+                free(cfile);
+                return;
+            }
+            memcpy(copy, (void *)mod[i].mod_start, len);
+            cfile->file.copy = mbfs_copyfile;
+            cfile->data = copy;
+            romfile_add(&cfile->file);
         }
-        memset(cfile, 0, sizeof(*cfile));
-        dprintf(1, "module %s, size 0x%x\n", (char *)mod[i].cmdline, len);
-        if (!extract_filename(cfile->file.name, (char *)mod[i].cmdline,
-                              sizeof(cfile->file.name))) {
-            free(cfile);
-            continue;
-        }
-        dprintf(1, "assigned file name <%s>\n", cfile->file.name);
-        cfile->file.size = len;
-        copy = malloc_tmp(len);
-        if (!copy) {
-            warn_noalloc();
-            free(cfile);
-            return;
-        }
-        memcpy(copy, (void *)mod[i].mod_start, len);
-        cfile->file.copy = mbfs_copyfile;
-        cfile->data = copy;
-        romfile_add(&cfile->file);
     }
+
+    multiboot_prepare_vga(mbi);
 }
